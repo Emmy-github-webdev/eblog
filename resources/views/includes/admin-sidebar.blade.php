74a442801dc873d24daf15e0131b6eb2b898ca91
Emmanuel <ogahemmanuel75@gmail.com>
2020-09-28 09:50:14 +0100

    Update admin-sidebar.blade.php

--- resources/views/includes/admin-sidebar.blade.php
+++ resources/views/includes/admin-sidebar.blade.php
@@ -12,7 +12,7 @@
         <li><a href="/admin"><i class="fa fa-tachometer fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
         <li><a href="/admin/users"><i class="fa fa-users fa-fw" aria-hidden="true"></i>&nbsp; Users</a></li>
         <li><a href="/admin/news/create"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add News</a></li>
-        <li><a href="/admin/article"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp; Manage Articles</a></li>
+        <li><a href="/admin/articles"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp; Manage Articles</a></li>
         <li><a href="/admin/news/categories"><i class="fa fa-compress" aria-hidden="true"></i>&nbsp;Categories</a></li>
         <li><a href="/admin/logout"><i class="fa fa-sign-out fa-fw" aria-hidden="true"></i>&nbsp; Logout</a></li>
         <li><a href="/login"><i class="fa fa-sign-in fa-fw" aria-hidden="true"></i>&nbsp; Sign</a></li>
